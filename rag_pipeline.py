df7ef8a1911d99d0c47d285c8cc52f06c9a7f3ca
sagarikasrinivasan <sagarika.srinivasan@gmail.com>
2026-04-30 22:48:13 -0400

    optional imports

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -5,3 +5,23 @@ import numpy as np
 from typing import List, Dict, Tuple, Optional
 from dataclasses import dataclass, field
 
+# optional imports
+
+try:
+    from sentence_transformers import SentenceTransformer
+    SBERT_AVAILABLE = True
+except ImportError:
+    SBERT_AVAILABLE = False
+
+try:
+    import faiss
+    FAISS_AVAILABLE = True
+except ImportError:
+    FAISS_AVAILABLE = False
+
+
+try:
+    from datasets import load_dataset
+    HF_AVAILABLE = True
+except ImportError:
+    HF_AVAILABLE = False
