3867fcc194f663fa3139313c9937fecd7ad18bdf
anushreerawal <anushreerawal7@gmail.com>
2026-04-30 22:16:16 -0400

    more stop words

--- baselines.py
+++ baselines.py
@@ -7,5 +7,14 @@ import numpy as np
 
 
 STOP_WORDS = {
-    "i", "me", "myself", "we", "our", "ours", "ourselves", "you", "your","yours", "he", "him", "his"
+    "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your",
+    "yours", "he", "him", "his", "she", "her", "hers", "it", "its", "they",
+    "them", "their", "what", "which", "who", "whom", "this", "that", "these",
+    "those", "am", "is", "are", "was", "were", "be", "been", "being", "have",
+    "has", "had", "do", "does", "did", "will", "would", "could", "should",
+    "may", "might", "must", "shall", "can", "a", "an", "the", "and", "but",
+    "or", "nor", "for", "so", "yet", "at", "by", "in", "of", "on", "to",
+    "up", "as", "into", "with", "about", "between", "through",
 }
+
+
